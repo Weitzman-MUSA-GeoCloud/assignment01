243a9a5197caff8f9777754bf13fa4e1c417153e
Ryan Swett <rswett01@design.upenn.edu>
2025-02-09 23:20:23 -0500

    Update query03.sql

--- query03.sql
+++ query03.sql
@@ -5,4 +5,9 @@
     `avg_duration`. Round to two decimal places.
 */
 
--- Enter your SQL query here
+SELECT 
+    AVG(duration) AS avg_trip_duration_2021
+FROM 
+    indego.trips_2021_q3;
+
+Result: 18.86 minutes
