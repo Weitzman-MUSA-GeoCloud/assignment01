7c0babcf34c7fda1e70eb197d2b0dc248f0e4758
Ryan Swett <rswett01@design.upenn.edu>
2025-02-10 23:01:33 -0500

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -8,7 +8,26 @@
     station (named `num_trips`).
 */
 
--- Enter your SQL query here
+SELECT
+    start_station AS station_id,
+    ST_AsText(ST_SetSRID(ST_MakePoint(start_lon, start_lat), 4326)) AS station_geog,  
+    COUNT(*) AS num_trips
+FROM (
+    SELECT start_time, start_station, start_lat, start_lon
+    FROM indego.trips_2021_q3
+    UNION ALL
+    SELECT start_time, start_station, start_lat, start_lon
+    FROM indego.trips_2022_q3
+) AS dat
+WHERE
+    EXTRACT(HOUR FROM start_time) BETWEEN 7 AND 9
+GROUP BY
+    start_station, start_lat, start_lon
+ORDER BY
+    num_trips DESC
+LIMIT 5;
+
+Result: Station IDs 3032, 3102, 3012, 3066, 3007
 
 
 /*
