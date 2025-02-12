64423783d5b991a9030b6575d008d69c5f1ba0f3
rmidge <rachelmidge@gmail.com>
2025-02-12 14:48:56 -0500

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -12,15 +12,17 @@
 -- Enter your SQL query here
 WITH trip_counts AS (
     SELECT 
-        (SELECT COUNT(*) FROM indego.trips_2021_q3) as count_2021,
-        (SELECT COUNT(*) FROM indego.trips_2022_q3) as count_2022
+        (SELECT COUNT(*) FROM indego.trips_2021_q3) AS count_2021,
+        (SELECT COUNT(*) FROM indego.trips_2022_q3) AS count_2022
 )
+
 SELECT 
     ROUND(
-        ((count_2022::float - count_2021::float) / count_2021::float * 100)::numeric,
+        ((count_2022::FLOAT - count_2021::FLOAT) / count_2021::FLOAT * 100)::NUMERIC,
         2
-    )::text || '%' as perc_change
+    )::TEXT || '%' AS perc_change
 FROM trip_counts;
+
 
 
 /*
