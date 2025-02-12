a579c327115b44e1b1d1c0137defec0927511a77
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 14:39:31 -0500

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -8,19 +8,23 @@
     station (named `num_trips`).
 */
 SELECT 
-    station_id, 
+    start_station, 
     ST_SetSRID(ST_MakePoint(start_lon, start_lat), 4326) AS station_geog, 
     COUNT(*) AS num_trips
-FROM indego.trips
+FROM (
+    SELECT start_station, start_lon, start_lat, start_time FROM indego.trips_2022_q3
+    UNION ALL
+    SELECT start_station, start_lon, start_lat, start_time FROM indego.trips_2021_q3
+) AS combined_trips
 WHERE 
     EXTRACT(HOUR FROM start_time) BETWEEN 7 AND 9
     AND EXTRACT(MINUTE FROM start_time) < 60
 GROUP BY 
-    station_id, 
+    start_station, 
     start_lon, start_lat
-ORDER BY num_trips DESC
+ORDER BY 
+    num_trips DESC
 LIMIT 5;
-
 /*
     Hint: Use the `EXTRACT` function to get the hour of the day from the
     timestamp.
