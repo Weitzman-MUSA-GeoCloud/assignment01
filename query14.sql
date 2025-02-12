1573c2b68f13295eb183742ed7dd241480637bce
Ryan Swett <rswett01@design.upenn.edu>
2025-02-11 21:55:04 -0500

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -6,4 +6,15 @@
     Meyerson Hall, rounded to the nearest 50 meters.
 */
 
--- Enter your SQL query here
+SELECT 
+    station_statuses.id AS station_id,
+    station_statuses.name AS station_name,
+    ROUND(ST_Distance(station_statuses.geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)) / 50) * 50 AS distance
+FROM 
+    indego.station_statuses
+ORDER BY 
+    distance ASC
+LIMIT 1;
+
+Result: 34th and Spruce (200 meters away)
+
