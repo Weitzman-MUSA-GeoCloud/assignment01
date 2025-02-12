26b72ade7d5f01173704c23c5606344a26b7da5a
Ryan Swett <rswett01@design.upenn.edu>
2025-02-11 21:50:18 -0500

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -6,4 +6,14 @@
     Meyerson Hall, rounded to the nearest 50 meters.
 */
 
--- Enter your SQL query here
+SELECT 
+    station_statuses.id AS station_id,
+    station_statuses.name AS station_name,
+    ROUND(ST_Distance(station_statuses.geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)) / 50) * 50 AS distance
+FROM 
+    indego.station_statuses
+ORDER BY 
+    distance DESC
+LIMIT 1;
+
+Result: Manayunk Bridge (8,900 meters away)
