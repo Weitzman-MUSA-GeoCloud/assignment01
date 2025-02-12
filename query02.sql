8a1f5b93fa8fa18287bdf9be6f0fe7d559f26aad
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 14:12:54 -0500

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -8,15 +8,11 @@
 
     Remember you can do calculations in the select clause.
 */
-
 SELECT 
     100.0 * ( 
         (SELECT COUNT(*) FROM indego.trips_2022_q3) - 
         (SELECT COUNT(*) FROM indego.trips_2021_q3)
     ) / (SELECT COUNT(*) FROM indego.trips_2021_q3) AS perc_change;
-
-
-
 /*
     If you want to get fancier here, you can cast the result to a string and
     concatenate a '%' to the end. For example:
