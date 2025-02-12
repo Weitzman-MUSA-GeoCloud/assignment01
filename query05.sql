b81344bd3c4c76e66754467453353fcbb378bec5
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 15:26:12 -0500

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -3,12 +3,7 @@
 
     Your result should have a single row with a single column named max_duration.
 */
-SELECT 
-    MAX(duration) AS max_duration
-FROM 
-    indego.trips_2021_q3 t
-JOIN 
-    indego.trips_2022_q3 t2
-ON
-    t.duration = t2.duration;
-
+SELECT MAX(t.duration) AS max_duration
+FROM indego.trips_2021_q3 t
+JOIN indego.trips_2022_q3 t2
+ON t.duration = t2.duration;
