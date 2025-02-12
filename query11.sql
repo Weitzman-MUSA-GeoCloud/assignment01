e4e60db2b02991d4833ca0a805dcb25b00ca9e28
Ryan Swett <rswett01@design.upenn.edu>
2025-02-11 21:38:29 -0500

    Update query11.sql

--- query11.sql
+++ query11.sql
@@ -4,4 +4,7 @@
     column named avg_distance_km.
 */
 
--- Enter your SQL query here
+SELECT 
+    ROUND(AVG(ST_Distance(station_statuses.geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)) / 1000)) AS avg_distance_km
+FROM 
+    public.station_statuses;
