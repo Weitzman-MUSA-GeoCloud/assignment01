62d32469a48fce8b61ec9dd04ea43ee87606eac0
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 15:15:01 -0500

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -6,23 +6,24 @@
     (named `station_id`), one for the point geography of the station (named
     `station_geog`), and one for the number of trips that started at that
     station (named `num_trips`).
-*/SELECT 
+*/
+SELECT 
     start_station AS station_id,  
-    ST_SetSRID(ST_MakePoint(start_lon, start_lat), 4326) AS station_geog, 
+    ST_SetSRID(ST_MakePoint(start_lat, start_lon), 4326) AS station_geog, 
     COUNT(*) AS num_trips
 FROM (
-    SELECT start_station, start_lon, start_lat, start_time FROM indego.trips_2021_q3
+    SELECT start_station, start_time 
+    FROM indego.trips_2021_q3
     UNION ALL
-    SELECT start_station, start_lon, start_lat, start_time FROM indego.trips_2022_q3
+    SELECT start_station, start_time 
+    FROM indego.trips_2022_q3
 ) AS combined_trips
 WHERE 
     EXTRACT(HOUR FROM start_time) BETWEEN 7 AND 9
-    AND EXTRACT(MINUTE FROM start_time) < 60
 GROUP BY 
     start_station, 
     start_lon, start_lat
-ORDER BY 
-    num_trips DESC
+ORDER BY num_trips DESC
 LIMIT 5;
 /*
     Hint: Use the `EXTRACT` function to get the hour of the day from the
