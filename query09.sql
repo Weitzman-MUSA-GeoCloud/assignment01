81ec1adfe22ddde538aba108af9c3132a11204d1
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 14:57:08 -0500

    Update query09.sql

--- query09.sql
+++ query09.sql
@@ -5,7 +5,6 @@
     and the number of trips taken by `passholder_type`. Your results should have
     two columns: `passholder_type` and `num_trips`.
 */
-
 SELECT
     passholder_type,
     COUNT(*) AS num_trips
@@ -15,11 +14,3 @@ FROM (
     SELECT passholder_type FROM indego.trips_2022_q3
 ) AS t
 GROUP BY passholder_type;
-
-Result: There are three passholder types. 
-1. Day Pass - 61,659
-2. Indego30 - 441,856
-3. Indego365 - 109,251
-
-However, 43 were coded as NULL and 2 were coded as Walk-up.
-
