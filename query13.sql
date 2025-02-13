7f02c658b2a78f39a1eee8c290de3dc01aaa924f
solanoamy <70601985+solanoamy@users.noreply.github.com>
2025-02-13 00:17:57 -0500

    queries 14 and 15 *but still duplicate alias*

--- query13.sql
+++ query13.sql
@@ -7,3 +7,17 @@
 */
 
 -- Enter your SQL query here
+with meyh_distances as (
+    select
+        id as station_id,
+        name as station_name,
+        round(public.st_distance(
+            geog,
+            public.st_makepoint(-75.192584, 39.952415)
+        ) / 50) * 50 as distance
+    from indego.station_statuses
+    group by station_id, station_name
+)
+select *
+from meyh_distances
+where distance = (select max(distance) from meyh_distances)
