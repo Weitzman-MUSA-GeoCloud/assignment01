cd9069f64ddecad8b9e0d9bfd512f04d59eab67a
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 21:10:48 -0500

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -4,10 +4,8 @@
     Your query should have a single record with a single attribute, the number
     of stations (num_stations).
 */
-
-SELECT 
-    COUNT(*) AS num_stations
-FROM 
+SELECT COUNT(*) AS num_stations
+FROM
     indego.station_statuses
-WHERE 
-    ST_Distance(station_statuses.geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)) <= 1000;
+WHERE
+    ST_DISTANCE(station_statuses.geog, ST_SETSRID(ST_MAKEPOINT(-75.192584, 39.952415), 4326)) <= 1000;
