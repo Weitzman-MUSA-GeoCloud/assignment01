e4c085305c67f961f79dc567d1da4068424abd3a
CuiyanWU <79962085+CuiyanWU@users.noreply.github.com>
2025-02-12 20:34:17 -0500

    Update db_structure.sql

--- db_structure.sql
+++ db_structure.sql
@@ -1,8 +1,42 @@
-/*
-    Example: How many bike trips in Q3 2021. Name the resulting column
-    num_trips.
-*/
+DROP TABLE IF EXISTS indego.trips_2021_q3;
+DROP TABLE IF EXISTS indego.trips_2022_q3;
 
--- Enter your SQL query here
-select count(*) as num_trips
-from indego.trips_2021_q3
+CREATE TABLE indego.trips_2021_q3 (
+    trip_id TEXT PRIMARY KEY,
+    duration INTEGER,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    start_station TEXT,
+    start_lat FLOAT,
+    start_lon FLOAT,
+    end_station TEXT,
+    end_lat FLOAT,
+    end_lon FLOAT,
+    bike_id TEXT,
+    plan_duration INTEGER,
+    trip_route_category TEXT,
+    passholder_type TEXT,
+    bike_type TEXT
+);
+
+CREATE TABLE indego.trips_2022_q3 (
+    trip_id TEXT PRIMARY KEY,
+    duration INTEGER,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    start_station TEXT,
+    start_lat FLOAT,
+    start_lon FLOAT,
+    end_station TEXT,
+    end_lat FLOAT,
+    end_lon FLOAT,
+    bike_id TEXT,
+    plan_duration INTEGER,
+    trip_route_category TEXT,
+    passholder_type TEXT,
+    bike_type TEXT
+);
+
+
+COPY indego.trips_2021_q3 FROM '/data/trips_2021_q3.csv' CSV HEADER;
+COPY indego.trips_2022_q3 FROM '/data/to/trips_2022_q3.csv' CSV HEADER;
