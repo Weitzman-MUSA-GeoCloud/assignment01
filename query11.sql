8affb68278608e3fc2cf77955f2c19a1b2179cd7
Ryan Swett <rswett01@design.upenn.edu>
2025-02-12 21:10:03 -0500

    Update query11.sql

--- query11.sql
+++ query11.sql
@@ -3,7 +3,14 @@
     from Meyerson Hall? Your result should have a single record with a single
     column named avg_distance_km.
 */
-
-SELECT 
-    ROUND(AVG(ST_Distance(station_statuses.geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)) / 1000)) AS avg_distance_km
+SELECT
+    ROUND(
+        AVG(
+            ST_DISTANCE(
+                station_statuses.geog,
+                ST_SETSRID(ST_MAKEPOINT(-75.192584, 39.952415), 4326)
+            )
+            / 1000
+        )
+    ) AS avg_distance_km
 FROM indego.station_statuses;
