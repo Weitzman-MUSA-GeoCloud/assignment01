a9dea61d6d016bf0928bb48d2ffb160da0d61d81
disandroa <adisandro818@gmail.com>
2025-02-14 15:50:44 -0500

    finished queary 12

--- query12.sql
+++ query12.sql
@@ -6,3 +6,14 @@
 */
 
 -- Enter your SQL query here
+with distances as (
+    select
+        statuses.id as station_id,
+        statuses.geog as station_geog,
+        round(st_distance(statuses.geog, st_setsrid(st_makepoint(-75.192584, 39.952415), 4326)::geography) / 50) * 50 as distance
+    from indego.station_statuses as statuses
+)
+select
+    count(*) as num_stations
+from distances
+where distance <= 1000;
