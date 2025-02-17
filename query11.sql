b850595923af70c4147222e9d44fa28d46b2fa59
rmidge <rachelmidge@gmail.com>
2025-02-16 19:27:04 -0500

    queries 10 and 11

--- query11.sql
+++ query11.sql
@@ -5,3 +5,13 @@
 */
 
 -- Enter your SQL query here
+SELECT
+    ROUND(
+        AVG(
+            ST_DISTANCE(
+                geog,
+                ST_SETSRID(ST_MAKEPOINT(-75.192584, 39.952415), 4326)::geography
+            )
+        ) / 1000
+    )::integer AS avg_distance_km
+FROM indego.station_statuses;
