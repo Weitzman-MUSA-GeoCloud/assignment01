a64eccdb7b37c8c72df71fd96fdaa11672f0be1e
rmidge <rachelmidge@gmail.com>
2025-02-16 19:41:06 -0500

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -7,3 +7,15 @@
 */
 
 -- Enter your SQL query here
+SELECT
+    id AS station_id,
+    name AS station_name,
+    ROUND(
+        ST_DISTANCE(
+            geog,
+            ST_SETSRID(ST_MAKEPOINT(-75.192584, 39.952415), 4326)::geography
+        ) / 50
+    ) * 50 AS distance
+FROM indego.station_statuses
+ORDER BY distance ASC
+LIMIT 1;
