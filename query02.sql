5a0ce39d87107b86e0f8be3373f9d2e2e381361d
Zhanchao Yang <zhanchao@upenn.edu>
2026-01-28 15:12:44 -0500

    no 2

--- query02.sql
+++ query02.sql
@@ -11,7 +11,24 @@
 
 -- Enter your SQL query here
 
-
+select count(*) as num_trips_2021
+from indego.trips_2021_q3;
+select count(*) as num_trips_2022
+from indego.trips_2022_q3;
+select
+    round(
+        (
+            (
+                (select count(*) from indego.trips_2022_q3)::decimal
+                -
+                (select count(*) from indego.trips_2021_q3)::decimal
+            )
+            /
+            (select count(*) from indego.trips_2021_q3)::decimal
+        )
+        * 100,
+        2
+    ) AS perc_change;
 
 /*
     If you want to get fancier here, you can cast the result to a string and
