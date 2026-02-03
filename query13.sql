b4444ee960e299e8805354251f7b1a4679a280e6
zyang91 <zhanchao@upenn.edu>
2026-02-02 19:55:49 -0500

    13 and 14

--- query13.sql
+++ query13.sql
@@ -7,3 +7,14 @@
 */
 
 -- Enter your SQL query here
+SELECT
+    station_statuses.id AS station_id,
+    station_statuses.name AS station_name,
+    ROUND(ST_Distance(
+        station_statuses.geog,
+        ST_SetSRID(
+            ST_MakePoint(-75.192584, 39.952415),4326)::geography
+    ) / 50) * 50 AS distance
+FROM indego.station_statuses
+ORDER BY distance DESC
+LIMIT 1;
