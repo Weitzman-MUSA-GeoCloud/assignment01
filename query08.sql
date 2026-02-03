600af05b5d2057a91edfaad9b671d3245d0ae704
zyang91 <zhanchao@upenn.edu>
2026-02-02 19:46:25 -0500

    08

--- query08.sql
+++ query08.sql
@@ -9,7 +9,27 @@
 */
 
 -- Enter your SQL query here
+SELECT
+    indego.stations_staues.id AS station_id,
+    indego.stations_staues.geog AS station_geog,
+    COUNT(*) AS num_trips
+FROM (
+    SELECT *
+    FROM indego.trips_2021_q3
+    WHERE EXTRACT(HOUR FROM start_time) BETWEEN 7 AND 9
 
+    UNION ALL
+
+    SELECT *
+    FROM indego.trips_2022_q3
+    WHERE EXTRACT(HOUR FROM start_time) BETWEEN 7 AND 9
+)
+AS combined_trips
+INNER JOIN indego.stations_staues
+    ON combined_trips.start_station_id = indego.stations_staues.id
+GROUP BY station_id, station_geog
+ORDER BY num_trips DESC
+LIMIT 5;
 
 /*
     Hint: Use the `EXTRACT` function to get the hour of the day from the
