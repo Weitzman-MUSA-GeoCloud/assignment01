133b88c629797b7956fd59c1c14814ec4cb2e9a2
mlingye <mlingye@upenn.edu>
2026-02-03 20:51:40 -0500

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -5,3 +5,9 @@
 */
 
 -- Enter your SQL query here
+select max(duration) as max_duration
+from (
+  select duration from indego.trips_2021_q3
+  union all
+  select duration from indego.trips_2022_q3
+) t;
