28a226b3bba463f68b0589c0d182382f2045e647
tess-vu <tessavu@proton.me>
2026-02-18 14:39:08 -0500

    linted query08.sql

--- query08.sql
+++ query08.sql
@@ -14,11 +14,17 @@ select
     indego.station_statuses.geog as station_geog,
     count(*) as num_trips
 from (
-    select start_station, start_time from indego.trips_2021_q3
+    select
+        start_station,
+        start_time
+    from indego.trips_2021_q3
     union all
-    select start_station, start_time from indego.trips_2022_q3
+    select
+        start_station,
+        start_time
+    from indego.trips_2022_q3
 ) as all_trips
-join indego.station_statuses
+inner join indego.station_statuses
     on all_trips.start_station::integer = indego.station_statuses.id
 where
     extract(hour from all_trips.start_time) between 7 and 9
