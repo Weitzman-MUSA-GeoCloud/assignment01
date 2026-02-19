4604089085539bead1415efc6dda3f41359c5d89
demiyang12 <demiyyq@gmail.com>
2026-02-19 14:13:04 -0500

    submission1

--- query11.sql
+++ query11.sql
@@ -5,3 +5,13 @@
 */
 
 -- Enter your SQL query here
+SELECT
+  ROUND(
+    AVG(
+      ST_Distance(
+        ST_SetSRID(s.geom, 4326)::geography,
+        ST_SetSRID(ST_Point(-75.192584, 39.952415), 4326)::geography
+      )
+    ) / 1000.0
+  ) AS avg_distance_km
+FROM indego.station_statuses s;
